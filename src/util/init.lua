0f023de53661efab63501ca7409540bc172e7e49
beadsland <beads.d.land@gmail.com>
2017-05-01 01:30:55 -0400

    utility peritable

--- src/util/init.lua
+++ src/util/init.lua
@@ -27,6 +27,9 @@ end
 
 Me.importmethods(Me, this, { 'isnan' } )
 
+local L = {}
+L.Peri = {}
+
 -- Close the door
 _ENV = nil
 
@@ -40,4 +43,34 @@ function Me.importmodules(parent, prefix, subs)
   for _, name in ipairs(parent) do
     parent[name] = require(prefix .. '.' .. name)
   end
+end
+
+---
+-- Access fields associated with, but not stored in, a table.
+--
+-- A peritable provides for quasi-protected scope without polluting
+-- either its table proper or that table's metatable. The keys of a
+-- peritable are not exposed for inspection, thus only those keys
+-- known to the caller are accessible.
+--
+-- On the other hand, a peritable makes explicit that its members are
+-- hidden, unlike the spooky action-at-a-distance of assigning an
+-- <code>__index</code> method to a table's metatable, nor the brute
+-- obfuscation of stashing values in that metatable.
+--
+-- @function peritable
+-- @param table A table to which a peritable is associated.
+-- @return The peritable associated with table.
+function Me.peritable(table, key, value)
+  local peri = L.Peri[table]
+  if not peri then
+    peri = {};
+    L.Peri[table] = peri
+
+    local hidden = {}
+    local mt = { __index    = function(_,k) return hidden[k] end,
+                 __newindex = function(_,k,v) hidden[k] = v end }
+    setmetatable(peri, mt)
+  end
+  return peri
 end
